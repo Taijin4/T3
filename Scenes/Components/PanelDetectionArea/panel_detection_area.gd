ce85ca91359e367fc94f27ad4a2390c7641a7411
CARLUCCI Romain <romain.carlucci@etu.unistra.fr>
2023-12-19 09:51:03 +0100

    + hover background for PanelDetectionArea

--- Scenes/Components/PanelDetectionArea/panel_detection_area.gd
+++ Scenes/Components/PanelDetectionArea/panel_detection_area.gd
@@ -2,9 +2,14 @@ extends Node2D
 
 signal panel_was_opened
 @onready var hover_label = $Bloc
+@onready var hover_polygon = $Polygon2D
 
 func _ready():
 	$Bloc/Label.text = get_meta("HoverText")
+	hover_polygon.polygon = $Area2D/CollisionPolygon2D.polygon
+	hover_polygon.color = Color(255, 255, 255, 0.3)
+	hover_polygon.position = $Area2D/CollisionPolygon2D.position
+	hover_polygon.hide()
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("left_mouse"):
@@ -12,9 +17,11 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 		get_node(get_meta("PanelToOpen")).show()
 	else:
 		hover_label.show()
+		hover_polygon.show()
 		var decalage : Vector2 = Vector2(-(hover_label.size.x/2), -(hover_label.size.y))
 		hover_label.set_position(get_global_mouse_position() + decalage)
 
 
 func _on_area_2d_mouse_exited():
 	hover_label.hide()
+	hover_polygon.hide()
