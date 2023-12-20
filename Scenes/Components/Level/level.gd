b7e871f79a2796d6a0caf488d35a34273acdf45a
CARLUCCI Romain <romain.carlucci@etu.unistra.fr>
2023-12-20 11:56:00 +0100

    ~ retrieving and processing data to match the new version of json & UI

--- Scenes/Components/Level/level.gd
+++ Scenes/Components/Level/level.gd
@@ -9,16 +9,29 @@ var level_data : Dictionary = {
 var locked : bool = true
 signal is_unlocked(col : String, niveau : int)
 
-func init(level : int, price : int, addition : String):
+func init(level : int, price : int, addition):
 	level_data['level'] = level
-	level_data['addition'] = addition
+	if typeof(addition) == TYPE_ARRAY:
+		for add in addition:
+			var sentence = add["sentence"].replace("{value}", str(add["value"]))
+			
+			var l1 := Label.new()
+			l1.text = sentence
+			l1.add_theme_font_size_override("font_size", 18)
+			l1.autowrap_mode = TextServer.AUTOWRAP_ARBITRARY
+			$Button/Margin/Locked/LockedAddition.add_child(l1)
+			
+			var l2 := Label.new()
+			l2.text = sentence
+			l2.add_theme_font_size_override("font_size", 18)
+			l2.autowrap_mode = TextServer.AUTOWRAP_ARBITRARY
+			$Button/Margin/Unlocked/UnlockedAddition.add_child(l2)
+	
 	# locked
-	find_child('LockedPrice').text = "----- $" + str(price) + "----- "
-	find_child('LockedAddition').text = addition
+	find_child('LockedPrice').text = "----- " + str(price) + "€ ----- "
 	
 	#unlocked
-	find_child('UnlockedPrice').text = "Niveau" + str(level)
-	find_child('UnlockedAddition').text = addition
+	find_child('UnlockedLevel').text = "Niveau " + str(level)
 
 
 func _on_button_pressed():
@@ -33,7 +46,7 @@ func enable():
 
 func unlock():
 	locked = not locked
-	$Button/Locked.hide()
-	$Button/Unlocked.show()
-	$Button.modulate = "#00FF00"
+	$Button/Margin/Locked.hide()
+	$Button/Margin/Unlocked.show()
+	$Button.self_modulate = "#00FF00"
 	is_unlocked.emit(level_data['level'])
