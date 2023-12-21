1662b11ca37659ae5d6712f04dc2c515cfa77e73
CARLUCCI Romain <romain.carlucci@etu.unistra.fr>
2023-12-21 11:21:45 +0100

    + signal that sends the level that has just been unlocked in LevelInitiator component

--- Scenes/Components/LevelInitiator/level_initiator.gd
+++ Scenes/Components/LevelInitiator/level_initiator.gd
@@ -1,6 +1,6 @@
 extends HBoxContainer
 
-
+signal level_unlocked(level : int)
 var levels_group_path = preload('res://Scenes/Components/VLevelsGroup/v_levels_group.tscn')
 var jsoncontroller = JsonController.new()
 
@@ -9,4 +9,8 @@ func init(panel_name : String):
 	for column in data:
 		var lg = levels_group_path.instantiate()
 		lg.init(column)
+		lg.level_unlocked.connect(_on_level_unlocked)
 		add_child(lg)
+
+func _on_level_unlocked(level : int):
+	level_unlocked.emit(level)
