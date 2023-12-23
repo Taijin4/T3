b9c8d261def32d6eec0b3b824f21d47f83f67e37
CARLUCCI Romain <romain.carlucci@etu.unistra.fr>
2023-12-23 01:30:33 +0100

    + real-time data update when user writes in the input in TextField component

--- Scenes/Components/TextField/text_field.gd
+++ Scenes/Components/TextField/text_field.gd
@@ -5,27 +5,23 @@ var allowed_chars = RegEx.new()
 @onready var materials_cost = {"wood" : 40, "hop" : 80, "ice" : 20, "other" : 1000, "beer" : 200}
 
 func _ready():
-	$content/title_container/title.text = get_meta("FieldTitle")
 	allowed_chars.compile("[^0-9]")
-	reset()
+	$content/title_container/title.text = get_meta("FieldTitle")
+	init()
+	
+	
+func init():
+	$content/input_container/LineEdit.text=""
+	update_price(0)
 
-func reset():
-	$content/input_container/LineEdit.text = "0"
-	set_price(0, get_meta("Prefix"))
 
-func set_price(new_price : int, prefix : String):
-	$content/price_container/price.text = prefix + " " + str(new_price) + "€"
+func update_price(number : int):
+	$content/price_container/price.text = get_meta("Prefix") + " " + str(number * materials_cost[get_meta("type")])
 
 
-func _on_line_edit_text_submitted(new_text):
+func _on_line_edit_text_changed(new_text):
 	if new_text == "" or allowed_chars.search(new_text):
-		new_text = "0"
-		$content/input_container/LineEdit.text = "0"
+		$content/input_container/LineEdit.delete_char_at_caret()
 	else:
-		set_price(materials_cost[get_meta("type")]*int(new_text), get_meta("Prefix"))
+		update_price(int(new_text))
 		new_text_submitted.emit(get_meta("type"), new_text)
-
-
-
-func _on_line_edit_focus_exited():
-	_on_line_edit_text_submitted($content/input_container/LineEdit.text)
